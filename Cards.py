f41a3af7a86f002d3fc83714a76603eff3b83de2
Alejandro Munoz <amunoz239@yahoo.com>
2020-06-24 21:33:58 -0500

    Adjusted Cards

--- Cards.py
+++ Cards.py
@@ -11,19 +11,15 @@ class Card:
         print(self.number)
     
     def PrintAll(self):
-        print(self.suit + " " + self.number)
+        print(self.suit,self.number ) 
+
 
 rank = ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
 suit = ["Clubs","Hears","Diamonds","Spades"]
 
 deck = []
 
-
-for x in rank:
-    for y in suit:
-       deck.append(Card(x,y))
-
-for x in deck:
-    print (x.GetNumber) 
-   
+for x in suit:
+    for y in rank:
+        deck.append(Card(x,y))
 

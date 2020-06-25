59cbf5419e50d70ec982c1a3ad19bc929ef85092
Alejandro Munoz <amunoz239@yahoo.com>
2020-06-25 11:25:51 -0500

    Added shuffle method and also added more to Deck file

--- Cards.py
+++ Cards.py
@@ -13,13 +13,25 @@ class Card:
     def PrintAll(self):
         print(self.suit,self.number ) 
 
+def BlackJackShuffle(aList):
+    length = len(aList)
+    middleNum = length//2 #Divides with inegral result
+    firstHalf = aList[:middleNum] # Gets first half
+    secondHalf = aList[middleNum:] #Gets second half
 
-rank = ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
-suit = ["Clubs","Hears","Diamonds","Spades"]
+    i = 0
+    j = 0
+    k = 0
+    for x in aList:
+        if i%2 == 0:
+            aList[i] = firstHalf[j]
+            j+=1
+        else:
+            aList[i] = secondHalf[k]
+            k+=1
+        i+=1
 
-deck = []
+    
 
-for x in suit:
-    for y in rank:
-        deck.append(Card(x,y))
+
 
--- Deck.py
+++ Deck.py
@@ -1,5 +1,22 @@
-import Cards.py
+from Cards import *
 
+rank = ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
+suit = ["Clubs","Hears","Diamonds","Spades"]
+
+deck = []
+
+for x in suit:# Creates the deck of cards 
+    for y in rank:
+        deck.append(Card(x,y))
+
+for x in deck:
+    x.PrintAll()
+
+BlackJackShuffle(deck)
+
+print("DECK SHUFFLED")
+for x in deck:
+    x.PrintAll()
 
 
 
